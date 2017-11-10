602844d5c7470147371dd12381bbeceda1d2e30d
xieydd <xieydd@gmail.com>
2017-11-10 08:16:59 +0800

    全矢Hiblert，画出希尔伯特包络，瞬时频率，希尔伯特频谱

--- FullSpectrum_CNN_Tensorflow/FullSpectrum/fv_hibert.py
+++ FullSpectrum_CNN_Tensorflow/FullSpectrum/fv_hibert.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 import scipy.signal as signals
 from scipy import fftpack
+import inspect
+import math
 
 #保证图像中文问题和符号问题
 matplotlib.rcParams['axes.unicode_minus']=False
@@ -111,9 +113,118 @@ def hibert(x,y):
    
 '''
 全矢希尔伯特  输入x_amplitude_envelope,y_amplitude_envelope
-y为0时即单通道数据
+xdata,ydata是两通道数据，当ydata数据为空或全为0时认为是单通道数据
+dir_sensor:水平方向传感器到垂直方向传感器的转向与旋转方向一致为1，相反为－1；
+angle_x:水平方向传感器与水平方向的夹角
+eps：计算误差要求
 vm,vs,vr,alpha,phase,wave_time:分别为主振矢，副振矢，振矢比，振矢角，矢相位和时域融合结果
 '''
-def fv_hibert(x,y):
+def fv_hibert(xdata,ydata,dir_sensor,angle_x,eps):
     
+    #输入变量检测
+    frame = inspect.currentframe()
+    args, _, _, values = inspect.getargvalues(frame)
+    nargin = len(args)
+    if nargin<1:
+        print('输入变量数不能小于1')
+    elif nargin==1:
+        n=len(xdata)
+        ydata=np.zeros(n,1)
+        dir_sensor=1
+        angle_x=0
+        eps=0.05
+    elif nargin==2:
+        dir_sensor=1
+        angle_x=0
+        eps=0.05
+    elif nargin==3:
+        angle_x=0
+        eps=0.05
+    elif nargin==4:
+        eps=0.05
+     
+    #判断输入单通道还是双通道
+    n =len(xdata)
+    flag_channel = 2
+    if y_data == None:
+        ydata = zeros(n,1)
+        flag_channel = 1
+    
+    #flag位为1时为正变换，为-1时为反变换
+    flag=1
+    vm = np.zeros(n/2,1)#定义主振矢
+    vs = np.zeros(n/2,1)#定义副振矢
+    alpha = np.zeros(n/2,1)#定义振矢角
+    rvN_k = np.zeros(n/2,1) 
+    ivN_k = np.zeros(n/2,1) 
+    phase = np.zeros(n/2,1) #定义矢相位，正进动相位角，反进动相位角
+    faia = np.zeros(n/2,1)
+    faib = np.zeros(n/2,1)
+    xdata = xdata-np.mean(xdata)
+    ydata = ydata-np.mean(ydata)
+    z = xdata+j*ydata
+    Z = 2*fftpack.fft(z)/n
+    rv=real(Z)
+    iv=imag(Z)
+    rvk = rv[1:n/2]
+    ivk = iv[1:n/2]
+    rvN_k[1]  = rv[1]
+    
+    for i in range(1,n/2):
+        rvN_k(i+1)=rv(n-i+1)
+        ivN_k(i+1)=iv(n-i+1)
+    vm[1]=0 #主振矢
+    vs[1]=0 #副振矢
+    alpha[1]=0 #振矢角
+    Xck=(rvN_k+rvk)/2
+    Xsk=(ivk-ivN_k)/2
+    Yck=(ivk+ivN_k)/2
+    Ysk=(rvN_k-rvk)/2
+    
+    zv=rv+j*iv
+    xp=0.5*np.abs(zv[2:n/2])      #正进动幅值序列
+    mxr=0.5*np.abs(zv[n/2+2:n]);   #反进动幅值序列所需中间变量
+    nn=len(mxr)            #反进动幅值序列长度
+    xr=np.zeros(nn,1)             #反进动幅值序列
+    tr=np.zeros(nn,1)
+    mmivN_k=np.zeros(nn,1)
+    mmrvN_k=np.zeros(nn,1)
+    
+    tanpk=iv[2:n/2]/np.linalg.inv(rv[2:n/2]) #正进动相位角
+    mtr=iv[n/2+2:n]/np.linalg.inv(rv[n/2+2:n]) #反进动相位角
+    for i in range(1,n+1):
+        xr[i]=mxr[nn-i+1]      #反进动幅值序列
+        tr[i]=mtr[nn-i+1]      #反进动相位角反向排序
+        mmivN_k[i]=iv[n-i+1]
+        mmrvN_k[i]=rv[n-i+1]
+        
+    #求主振矢、副振矢、振矢比
+    vm[2:n/2]=xp+xr           #求主振矢
+    vs[2:n/2]=dir_sensor*(xp-xr) #求副振矢，考虑传感器安装方向与转速方向
+    vr=vs/np.linalg.inv(vm)   #振矢比的值域为[-1，1]；
+    bb=(iv[2:n/2]*mmrvN_k+mmivN_k*rv[2:n/2])#./(rv(2:n/2).*mmrvN_k);
+    aa=(rv[2:n/2]*mmrvN_k-iv[2:n/2]*mmivN_k)#./(rv(2:n/2).*mmrvN_k);
+    atan2a=math.atan(bb/np.linalg.inv(aa))
+    #根据2a所在象限调整2a的值
+    for i in range(1,n/2):
+        if (aa[i]<0 and bb[i]<0):                #2a位于第三象限时
+            atan2a[i]=atan2a[i]+pi
+        elif (aa[i]<0 and bb[i])>0:            #2a位于第二象限时
+            atan2a[i]=atan2a[i]+pi
+        elif (aa[i]>0 and bb[i]<0):            #2a位于第四象限时
+            atan2a[i]=atan2a[i]+2*pi
+    
+    #计算振矢角
+    alpha[2:n/2]=0.5*atan2a*180/pi            #通过2a算振矢角a，单位：角度,值域为[0，180]
+    alpha=alpha+angle_x                       #把相角从与X方向夹角变换到与水平方向夹角
+    for i in range(1,n/2+1):
+        if (alpha[i]>180):
+            alpha[i]=alpha[i]-180
+    #计算矢相位
+    for i in range(1,n/2+1):
+        faia[i]=math.atan2(Xsk[i]*cos(alpha[i])+Ysk[i]*sin(alpha[i]),Xck[i]*cos(alpha[i])+Yck[i]*sin(alpha[i])) # 设xr1=vm*cos(omega*t+faia1)
+        faib[i]=math.atan2(-Xsk[i]*sin(alpha[i])+Ysk[i]*cos(alpha[i]),-Xck[i]*sin(alpha[i])+Yck[i]*cos(alpha[i])) #设yr1=vs*cos(omega*t+faib1)  把椭圆方程化成标准形式
+    fai=faia-faib
+    phase=math.atan(ivk/np.linalg.inv(rvk))*180/pi #矢谱分析技术中的相位角
+    #根据相角所在象限调整矢相角值
     
