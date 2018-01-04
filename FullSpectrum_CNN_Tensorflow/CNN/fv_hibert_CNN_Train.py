ce8e3d1a9b72891fc6f334491b5bb33a8a0373c0
xieydd <xieydd@gmail.com>
2018-01-04 16:13:55 +0800

    运行全矢希尔伯特的CNN模型,已经可以在Tensorboard上显示出来，使用Matlab的tsne进行特征的可视化一维二维效果很好，但是python不能很好的画出tsne存在问题，下一步看如何将具体的数据显示在PROJECTORS中

--- FullSpectrum_CNN_Tensorflow/CNN/fv_hibert_CNN_Train.py
+++ FullSpectrum_CNN_Tensorflow/CNN/fv_hibert_CNN_Train.py
@@ -8,10 +8,18 @@ import os
 import pandas as pd
 import sys
 import time
+import scipy
 from datetime import timedelta
 from sklearn.model_selection import train_test_split
 from fv_hibert_CNN_Model import FV_Hilbert_CNNConfig,FV_Hilbert_CNN
 from sklearn import metrics
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
+from sklearn import (manifold, datasets, decomposition, ensemble,
+                     discriminant_analysis, random_projection)
+from mpl_toolkits.mplot3d import Axes3D
 
 filenames = 'J:/1/啦啦啦/sample'
 save_dir = 'D:\Graph'
@@ -101,11 +109,14 @@ def evaluate(sess,x_,y_):
 
 
 
+
+
 def train(signal,labels):
     print('Configuting TensorBoard and Saver...')
     # 配置 Tensorboard，重新训练时，请将tensorboard文件夹删除，不然图会覆盖
     #有玄学，必须使用\不然报错PermissionDeniedError: Failed to create a directory，而且C盘好像还不行，服气
     tensorboard_dir = 'D:\Graph'
+    t_sne = None
     if not os.path.exists(tensorboard_dir):
         os.makedirs(tensorboard_dir)
    
@@ -116,7 +127,7 @@ def train(signal,labels):
 
     merged_summary = tf.summary.merge_all()
     writer = tf.summary.FileWriter(tensorboard_dir)
-   
+    
     #配置Saver
     saver = tf.train.Saver()
     if not os.path.exists(save_dir):
@@ -138,6 +149,7 @@ def train(signal,labels):
     last_improved = 0          #记录上一次提升批次
     required_improved = 1000   #如果超过1000lunix不提升就提前结束
    
+    
     flag = False
     for epoch in range(config.num_epochs):
         x_train,x_test,y_train,y_test=train_test_split(signal,labels,test_size=0.2,random_state=0)
@@ -171,7 +183,61 @@ def train(signal,labels):
                         + ' Val Loss: {3:>6.2}, Val Acc: {4:>7.2%}, Time: {5} {6}'
                 print(msg.format(total_batch, loss_train, acc_train, loss_val, acc_val, time_dif, improved_str))
             session.run(model.train_step,feed_dict = feed_dict)
+            
+            
+            x_train_reshaped = np.reshape(x_train,[-1,32,32,1])
+            feed_tsne = feed_data(x_train_reshaped,y_train,config.drop_out)
+            t_sne,cValus = session.run([model.logit_1,model.input_y],feed_dict=feed_tsne)
+            
+            if total_batch == 3000:
+                #scipy.io.savemat('J:/1/啦啦啦/t-sne/arg_label.mat',{'arg_label':y_train})
+                #scipy.io.savemat('J:/1/啦啦啦/t-sne/arg_logit.mat',{'arg_logit':t_sne})
+                #scipy.io.savemat('J:/1/啦啦啦/t-sne/y_pred_cls.mat',{'y_pred_cls':t_sne})
+                #x_train_reshaped = np.reshape(x_train,[-1,32,32,1])
+                #feed_tsne = feed_data(x_train_reshaped,y_train,config.drop_out)
+                '''
+                t_sne = np.zeros((t_sne_1.shape))
+                for i in range(t_sne_1.shape[0]):
+                   a = t_sne_1[i,:].max()
+                   for j in range(t_sne_1.shape[1]):
+                      if (t_sne_1[i][j] == a):
+                         t_sne[i][j] = 1.0
+                      else:
+                         t_sne[i][j] = 0.0
+                print(t_sne)
+                '''
+                X_tsne = TSNE(n_components = 3,learning_rate=50).fit_transform(t_sne)
+                X_pca = PCA(n_components=3).fit_transform(t_sne)
+                
+                '''
+                import seaborn as sns
+                palette = np.array(sns.color_palette("hls", 10))
+                c=palette[colors.astype(np.int)]
+                
+                
+                X_tsne_min, X_tsne_max = np.min(X_tsne,axis=0), np.max(X_tsne,axis=0)
+                X_tsne = (X_tsne - X_tsne_min) / (X_tsne_max - X_tsne_min)
+                
+                
+                X_pca_min, X_pca_max = np.min(X_pca,axis=0), np.max(X_pca,axis=0)
+                X_pca = (X_pca - X_pca_min) / (X_pca_max - X_pca_min)
+                '''
+                
+                cValue = ['#7e1e9c','#15b01a','#0343df','#ff81c0','#653700','#e50000','#95d0fc','#029386','#f97306']
+                fig = plt.figure(figsize=(10, 5))
+                ax1 = fig.add_subplot(121, projection='3d')
+                print(cValus[:,0])
+                ax1.scatter(X_tsne[:, 0], X_tsne[:, 1],X_tsne[:, 2],c=cValus[:,0])
+                ax1.set_title('T-SNE')
+                ax2 = fig.add_subplot(122, projection='3d')
+                ax2.scatter(X_pca[:, 0], X_pca[:, 1],X_pca[:, 2],c=cValus[:,0])
+                ax2.set_title('PCA')
+                
+                plt.show()
+                plt.savefig("examples.jpg") 
+                
             total_batch += 1
+            
             if total_batch - last_improved > required_improved:
                 print("No Optimization for a long time,auto-stopping.....")
                 flag = True
@@ -179,12 +245,11 @@ def train(signal,labels):
         if flag:
             break
       
-      
 def test(signal,labels):
     print("Loading test data...")
     x_train,x_test,y_train,y_test=train_test_split(signal,labels,test_size=0.2,random_state=0)
     x_train = np.reshape(x_train,[-1,32,32,1])
-    x_test = np.reshape(x_train,[-1,32,32,1])
+    x_test = np.reshape(x_test,[-1,32,32,1])
     start_time = time.time()
     session = tf.Session()
     session.run(tf.global_variables_initializer())
@@ -213,6 +278,7 @@ def test(signal,labels):
         
        #评价指标
         print("Precision,Recall and F1-Score...")
+        categories = ['0','1','2','3','4','5','6','7','8']
         print(metrics.classification_report(y_test_cls,y_pred_cls,target_names=categories))
 
         #混淆函数
@@ -227,7 +293,7 @@ def test(signal,labels):
 
 
 
-
+'''
 def main(argv=None):
    signal,labels = load_data(filename)
    train(signal,labels)
@@ -243,3 +309,14 @@ if __name__ == 'main':
       train()
    else:
       test()
+'''
+
+def main(argv=None):
+    signal,labels = load_data(filenames)
+    train(signal,labels)
+    #test(signal,labels)
+    
+
+config = FV_Hilbert_CNNConfig()
+model = FV_Hilbert_CNN(config)
+main()
